c5cc9a0ef4ede0c4ebbb9dcc3a3da69c26dfc3d0
Tri Hoang <hmtri1011@gmail.com>
2020-11-18 14:24:44 +0700

    feat: test custom hook

---
+++ packages/client/src/hooks/useLocation.test.tsx
@@ -0,0 +1,39 @@
+import { renderHook } from '@testing-library/react-hooks'
+
+import useLocation from './useLocation'
+
+const mockData = {
+  location: {
+    title: 'London'
+  }
+}
+
+beforeEach(() => {
+  // @ts-ignore
+  fetch.resetMocks()
+})
+
+describe('useLocation hook', () => {
+  it('should show loading state', () => {
+    // @ts-ignore
+    fetch.mockResponseOnce(JSON.stringify(mockData))
+    const { result } = renderHook(() => useLocation('london'))
+
+    expect(result.current.loading).toBe(true)
+  })
+
+  it('hide loading when have data', async () => {
+    // @ts-ignore
+    fetch.mockResponseOnce(JSON.stringify(mockData))
+
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useLocation('london')
+    )
+
+    expect(result.current.loading).toBe(true)
+
+    await waitForNextUpdate()
+    expect(result.current.loading).toBe(false)
+    expect(result.current.data?.title).toBe('London')
+  })
+})
